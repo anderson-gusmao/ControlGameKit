4c2a48e15df690c15d21d1232e01a3c0c7b7fd44
Anderson Santos Gusmão <anderson.santos.gusmao@gmail.com>
2020-08-30 15:39:55 -0300

    Add more unit tests to increase code coverage.

---
+++ UnitTests/DeallocationTests.swift
@@ -0,0 +1,23 @@
+//
+//  DeallocationTests.swift
+//  ControlGameKitTests
+//
+//  Created by Anderson Santos Gusmão on 30/08/20.
+//  Copyright © 2020 Anderson Santos Gusmão. All rights reserved.
+//
+
+import XCTest
+import GameController
+import Combine
+@testable import ControlGameKit
+
+class DeallocationTests: XCTestCase {
+
+	var joystick: Joystick?
+
+    func testDeinit() throws {
+		joystick = Joystick()
+		joystick = nil
+		XCTAssertNil(joystick)
+    }
+}
